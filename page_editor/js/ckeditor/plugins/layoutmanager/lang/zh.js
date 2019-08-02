1dd03bab5d37b24346a25ea524f8bdfcd5b4a106
ray831 <ray831@users.noreply.github.com>
2019-08-02 12:50:46 +0800

    Init

---
+++ page_editor/js/ckeditor/plugins/layoutmanager/lang/zh.js
@@ -0,0 +1,7 @@
+CKEDITOR.plugins.setLang('layoutmanager', 'zh', {
+    title: 'Layouts',
+    addLayoutDialogTitle: '選擇 layout',
+    manageLayoutDialogTitle: '替換成',
+    removeLayoutMenuLabel: '移除 layout',
+    manageLayoutMenuLabel: '替換 layout'
+});
